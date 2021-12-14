0d0a521a12c4d46f8f5b15450d9def0d412c6f83
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2021-12-14 13:45:06 -0300

    Unit test withdraw sem saldo positivo

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -56,4 +56,59 @@ describe('Create statement.', () => {
       });
     }).rejects.toBeInstanceOf(AppError);
   });
+
+  it('Should be able to create withdraw in statement to user with positive balance.', async () => {
+    const user = await createUser('User test', 'teste@teste.com', 'passTest');
+
+    const { id } = user;
+
+    await createStatementUseCase.execute({
+      user_id: id as string,
+      type: 'deposit' as any,
+      amount: 10,
+      description: 'test',
+    });
+
+    const withdrawStatement = await createStatementUseCase.execute({
+      user_id: id as string,
+      type: 'withdraw' as any,
+      amount: 5,
+      description: 'test',
+    });
+
+    expect(withdrawStatement).toHaveProperty('id');
+  });
+
+  it('Should not be able to create withdraw in statement with not positive balance.', async () => {
+    const user1 = await createUser('User test', 'teste@teste.com', 'passTest');
+    const user2 = await createUser('User test 1', 'teste1@teste.com', 'passTest');
+
+    const { id: id1 } = user1;
+    const { id: id2 } = user2;
+
+    await createStatementUseCase.execute({
+      user_id: id2 as string,
+      type: 'deposit' as any,
+      amount: 10,
+      description: 'test',
+    });
+
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id: id1 as string,
+        type: 'withdraw' as any,
+        amount: 10,
+        description: 'test',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id: id2 as string,
+        type: 'withdraw' as any,
+        amount: 11,
+        description: 'test',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
 })
