b30ddc573e3dd23ebcd91e2b9e6bc55e171d694d
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2021-12-14 11:41:23 -0300

    Definição de database test

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (host = 'dbpostgres'): Promise<Connection> => {
       host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'rentx_test'
+          ? 'fin_api_test'
           : defaultOptions.database,
     }),
   );
